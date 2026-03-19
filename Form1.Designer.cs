519a0933021dd8acab267d99ea63d750807e5cf7
양채현 <2423978@naver.com>
2026-03-19 17:04:44 +0900

    과제 1 완료 txtbx에 입력된 문장을 전송버튼을 통해 리스트 박스로 보내기

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            title = new Label();
+            lstbx = new ListBox();
+            btn = new Button();
+            txtbx = new TextBox();
             SuspendLayout();
+            // 
+            // title
+            // 
+            title.AutoSize = true;
+            title.Font = new Font("맑은 고딕", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            title.ForeColor = Color.DarkBlue;
+            title.Location = new Point(30, 21);
+            title.Name = "title";
+            title.Size = new Size(296, 50);
+            title.TabIndex = 0;
+            title.Text = "EchoMessenger";
+            // 
+            // lstbx
+            // 
+            lstbx.FormattingEnabled = true;
+            lstbx.Location = new Point(30, 97);
+            lstbx.Name = "lstbx";
+            lstbx.Size = new Size(654, 214);
+            lstbx.TabIndex = 1;
+            // 
+            // btn
+            // 
+            btn.BackColor = SystemColors.ActiveCaption;
+            btn.ForeColor = Color.DarkBlue;
+            btn.Location = new Point(538, 351);
+            btn.Name = "btn";
+            btn.Size = new Size(146, 45);
+            btn.TabIndex = 2;
+            btn.Text = "전송";
+            btn.UseVisualStyleBackColor = false;
+            btn.Click += btn_Click;
+            // 
+            // txtbx
+            // 
+            txtbx.Location = new Point(30, 351);
+            txtbx.Multiline = true;
+            txtbx.Name = "txtbx";
+            txtbx.Size = new Size(500, 45);
+            txtbx.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(744, 454);
+            Controls.Add(txtbx);
+            Controls.Add(btn);
+            Controls.Add(lstbx);
+            Controls.Add(title);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label title;
+        private ListBox lstbx;
+        private Button btn;
+        private TextBox txtbx;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace EchoMessenger
 {
     public partial class Form1 : Form
@@ -10,6 +12,13 @@ namespace EchoMessenger
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string typed_msg;
+            typed_msg = txtbx.Text;
+            lstbx.Items.Add(typed_msg);
         }
     }
 }
